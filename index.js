3d576c8d2a23bcefc05c8fe010ebd5aa13c70918
Amit Sharma <amittsharma4567@gmail.com>
2025-07-06 14:42:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ let currFamilyId = 0;
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/callback"
+  callbackURL: "https://travella-jaio.onrender.com/auth/google/callback"
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     const email = profile.email;
